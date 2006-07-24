37a6189be30bd7329270ed9696d85cbd1c7b00d4
kosukeimai <kimai@princeton.edu>
2006-07-24 19:43:54 +0000

    adding Bayesian regression

--- src/models.c
+++ src/models.c
@@ -75,6 +75,88 @@ void logitMetro(int *Y,        /* outcome variable: 0, 1, ..., J-1 */
   free(prop);
 }
 
+/* Bayesian normal regression */
+void bNormalReg(double *Y,     /* response variable */
+		double **X,    /* model matrix */
+		double *beta,  /* coefficients */
+		double sig2,   /* variance */
+		int n_samp,    /* sample size */
+		int n_cov,     /* # of covariates */
+		int prior,     /* 0: improper prior 
+				  p(beta,sigma^2|X) \propto sigma^{-2}
+				  1: proper prior for (beta, sigma^2)
+				  p(beta|X) = normal(beta0, A0)
+				  p(sigma2|X) = InvChi2(nu0, s0)
+			       */
+		double *beta0, /* prior mean for normal */
+		double **A0,   /* prior precision for normal; can be
+				  set to zero to induce improper prior
+				  for beta alone
+			       */
+		double s0,  /* prior scale for InvChi2 */
+		int nu0     /* prior d.f. for InvChi2 */
+		) {
+  /*** model parameters ***/
+  double **SS = doubleMatrix(n_cov+1, n_cov+1); /* matrix folders for SWEEP */
+  double *mean = doubleArray(n_cov);            /* means for beta */
+  double **V = doubleMatrix(n_cov, n_cov);      /* variances for beta */
+  double **mtemp = doubleMatrix(n_cov, n_cov);
+
+  /*** storage parameters and loop counters **/
+  int i, j, k;  
+  
+  /*** read the proper prior as additional data points ***/
+  if (prior) {
+    dcholdc(A0, n_cov, mtemp);
+    for(i = 0; i < n_cov; i++) {
+      X[n_samp+i][n_cov] = 0;
+      for(j = 0; j < n_cov; j++) {
+	X[n_samp+i][n_cov] += mtemp[i][j]*beta0[j];
+	X[n_samp+i][j] = mtemp[i][j];
+      }
+    }
+  }
+  for (i = 0; i < n_samp; i++)
+    X[i][n_cov] = Y[i];
+
+  /* SS matrix */
+  for(j = 0; j <= n_cov; j++)
+    for(k = 0; k <= n_cov; k++)
+      SS[j][k]=0;
+  for(i = 0;i < n_samp; i++)
+    for(j = 0;j <= n_cov; j++)
+      for(k = 0; k <= n_cov; k++) 
+	SS[j][k] += X[i][j]*X[i][k];
+  if (prior) 
+    for(i = n_samp;i < n_samp+n_cov; i++)
+      for(j = 0;j <= n_cov; j++)
+	for(k = 0; k <= n_cov; k++) 
+	  SS[j][k] += X[i][j]*X[i][k];
+  
+  /* SWEEP SS matrix */
+  for(j = 0; j < n_cov; j++)
+    SWP(SS, j, n_cov+1);
+
+  /* draw sig2 and then beta */    
+  for(j = 0; j < n_cov; j++)
+    mean[j] = SS[j][n_cov];
+  if (prior)
+    sig2=(SS[n_cov][n_cov]+s0)/rchisq((double)n_samp+nu0);
+  else
+    sig2=SS[n_cov][n_cov]/rchisq((double)n_samp-n_cov);
+  
+  for(j = 0; j < n_cov; j++)
+    for(k = 0; k < n_cov; k++) V[j][k]=-SS[j][k]*sig2;
+  rMVN(beta, mean, V, n_cov);
+  
+  /** freeing memory **/
+  free(mean);
+  FreeMatrix(SS, n_cov+1);
+  FreeMatrix(V, n_cov);
+  FreeMatrix(mtemp, n_cov);
+}
+
+
 /* A Gibbs sampler for binary probit regression with and without
    marginal data augmentation 
 */ 
@@ -177,45 +259,4 @@ void bprobitGibbs(int *Y,        /* binary outcome variable */
 
 } 
 
-/* A routine used for Mixed Gaussian Model */
-void compW(double **T, double sig2, double **X, int start, int size,
-	   double *W_vec, int n, int p);
-void compW(double **T, double sig2, double **X, int start, int size,
-	   double *W_vec, int n, int p){
 
-  int i,j,k;
-  double **V = doubleMatrix(size, size);
-  double **TEMP = doubleMatrix(size, p+1);
-  double **W = doubleMatrix(size, size);     /* The weight matrix */
-  double **W_inv = doubleMatrix(size, size); /* inverse of the weight matrix */
-  
-  for(i=0;i<size;i++){
-    for(j=0;j<size;j++)V[i][j]=0;
-    for(j=0;j<=p;j++)TEMP[i][j]=0;
-  }
-  for(i=0;i<size;i++)     /* row of X */
-    for(j=0;j<=p;j++)   /* col of T */
-      for(k=0;k<=p;k++)
-	TEMP[i][j]+=T[k][j]*X[start+i][k];
-  for(i=0;i<size;i++)     /* row of TEMP */
-    for(j=0;j<size;j++)   /* col of t(X) */
-      for(k=0;k<=p;k++)
-	V[i][j]+=TEMP[i][k]*X[start+j][k];
-  for(i=0;i<size;i++){
-    for(j=0;j<size;j++) W_inv[i][j]=V[i][j];
-    W_inv[i][i]+=sig2;
-  }
-  dinv(W_inv,size,W);
-
-  for(j=0;j<size;j++)             /* Store W  */
-    for(k=0;k<size;k++)
-      W_vec[j*size+k]=W[j][k];
-
-  FreeMatrix(V, size);
-  FreeMatrix(TEMP, size);
-  FreeMatrix(W, size);
-  FreeMatrix(W_inv, size);
-}
-
-/* Multinomial Probit model from MNP: 
-   It only works when */
--- src/models.h
+++ src/models.h
@@ -1,6 +1,10 @@
 void logitMetro(int *Y, double **X, double *beta, int n_samp,      
 		int n_dim, int n_cov, double *beta0, double **A0,     
 		double *Var, int n_gen, int *counter);
+
+void bNormalReg(double *Y, double **X, double *beta, int n_samp,
+		int n_cov, int prior, double *beta0, double **A0,
+		double s0, int nu0);
   
 void bprobitGibbs(int *Y, double **X, double *beta, int n_samp, 
 		  int n_cov, int prior, double *beta0, double **A0, 
