d8e9d46de7e86eb8cd780f0816349fd8ea0bcd2a
kosukeimai <kimai@princeton.edu>
2006-09-25 19:57:06 +0000

    up

--- R/ITTnocov.R
+++ R/ITTnocov.R
@@ -9,7 +9,7 @@ ITTnocov <- function(Y, Z, grp = NULL){
   if (is.null(grp))
     ITT.var <- var(Y[Z==1])/sum(Z==1) + var(Y[Z==0])/sum(Z==0)
   else
-    ITT.var <- varCluster(Y[Z==1], grp[Z==0])$var +
+    ITT.var <- varCluster(Y[Z==1], grp[Z==1])$var +
       varCluster(Y[Z==0], grp[Z==0])$var
 
   return(list(est = ITT, var = ITT.var))
