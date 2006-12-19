9fcfc8f2d016400364a39d9e6dcd317b8b536ec0
kosukeimai <kimai@princeton.edu>
2006-12-19 14:46:39 +0000

    up

--- R/print.randomize.R
+++ R/print.randomize.R
@@ -1,13 +1,13 @@
 print.randomize <- function(x, digits = getOption("digits"), ...) {
 
   cat("\nCall:\n", deparse(x$call), "\n\n", sep = "")
-  if (!is.null(block))
-    print(ftable(object$block, object$treatment, exclude = NULL, ...),
+  if (!is.null(x$block))
+    print(ftable(x$block, x$treatment, exclude = NULL, ...),
           digits = digits)
   else
-    print(table(object$treatment, exclude = NULL, ...), digits = digits)
+    print(table(x$treatment, exclude = NULL, ...), digits = digits)
 
   cat("\nTotal number of observations:",
-      length(na.rm(object$treatment)), "\n\n")
+      length(na.rm(x$treatment)), "\n\n")
   invisible(x)
 }
