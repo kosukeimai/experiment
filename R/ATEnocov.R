b6de3f8b3b5fa87337b313765f86e5248939ecdf
kosukeimai <kimai@princeton.edu>
2007-03-06 04:10:15 +0000

    up

--- R/ATEnocov.R
+++ R/ATEnocov.R
@@ -1,15 +1,18 @@
 ###
-### Calculate the ATE from cluster randomized experiments
+### Calculate the ATE 
 ###
-### unbiased = TRUE: my own estimator 
-### unbiased = FALSE: standard estimator using the ``average method''
-###            For matched-pair designs, see Donner and Klar
-###            (Statistics in Medicine, 1987; Journal of Clinical Epidemiology, 1993)
-###            A simple weight in Donner and Klar (1993) is used.
+### grp.method: method for group-randomized trials
+###   "neyman" = my own estimator 
+###   "textbook" = the method in Chapter 7 of Donner and Klar (2000)
+###                (see also their 1993 paper in Journal of Clinical Epidemiology)
+###   "unpooled" = an unpooled version of the "textbook" method (this option
+###                is not available for matched-pair designs)
+###   "standard" = the "standard" method conditioning on the number of
+###                units in the treatment and control groups
 ###
 
 ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
-                     match = NULL, size = NULL, unbiased = TRUE){
+                     match = NULL, grp.size = NULL, grp.method = "neyman"){
 
   ## an internal function that checks match and returns diff
   match.check <- function(Y, Z, match) { 
@@ -33,15 +36,17 @@ ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
   Y <- eval(call$Y, envir = data)
   Z <- eval(call$Z, envir = data)
   grp <- eval(call$grp, envir = data)
-  size <- eval(call$size, envir = data)
+  grp.size <- eval(call$grp.size, envir = data)
   match <- eval(call$match, envir = data)
 
-  ## checking Z
+  ## checking data
   if (sum(sort(unique(Z)) == c(0,1)) != 2)
     stop("`Z' should be binary taking the value of 0 or 1")
-
+  if (grp.method %in% c("neyman", "textbook", "unpooled", "standard"))
+    stop("invalid input for `grp.method'")
+  
   ## ATE for unit randomization 
-  if (is.null(grp) && is.null(size)) { 
+  if (is.null(grp) && is.null(grp.size)) { 
     ATE.est <- mean(Y[Z==1])-mean(Y[Z==0])
     if (is.null(match)) { # without matching
       ATE.var <- var(Y[Z==1])/sum(Z==1)+var(Y[Z==0])/sum(Z==0)
@@ -52,22 +57,22 @@ ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
     return(list(est = ATE.est, var = ATE.var, Y = Y, Z = Z, match = match))
   }
 
-  ## ATE for cluster randomizaion
+  ## ATE for group-randomized trials
   if (is.null(grp)) { # aggregate data input
-    if (!unbiased && !is.null(match)) { 
+    if (!(grp.method %in% c("neyman", "standard")) && !is.null(match)) { 
       stop("the input should be individual-level data for this estimator")
     } else {
       M <- length(Y)
-      Ysum <- Y*size
+      Ysum <- Y*grp.size
     } 
-  } else if (unbiased || !is.null(match)) { # individual data input
+  } else if (grp.method == "neyman" || !is.null(match)) { # individual data input
     ugrp <- unique(grp)
     M <- length(ugrp)
-    if (is.null(size)) {
+    if (is.null(grp.size)) {
       tmp <- Z
-      Ysum <- Z <- size <- rep(NA, M)
+      Ysum <- Z <- grp.size <- rep(NA, M)
       for (i in 1:M) {
-        size[i] <- sum(grp == ugrp[i])
+        grp.size[i] <- sum(grp == ugrp[i])
         Ysum[i] <- sum(Y[grp == ugrp[i]])
         Zvalue <- unique(tmp[grp == ugrp[i]]) 
         if (length(Zvalue) != 1)
@@ -90,12 +95,12 @@ ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
         }
       }
     } else {
-      stop("`size' should be NULL when `grp' is specified")
+      stop("`grp.size' should be NULL when `grp' is specified")
     }
   }
   
-  if (unbiased) { ## my method
-    N <- sum(size)
+  if (grp.method == "neyman") { ## my method
+    N <- sum(grp.size)
     m1 <- sum(Z)
     m0 <- M-m1
     if (is.null(match)) { # without matching
@@ -107,18 +112,18 @@ ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
       ATE.var <- 2*M*var(diff)/(N^2)
     }
     return(list(est = ATE.est, var = ATE.var, Ysum = Ysum, Z = Z,
-                size = size, match = match, unbiased = unbiased))  
-  } else { ## Donner and Klar
-    if (is.null(match)) { # average method 
+                grp.size = grp.size, match = match, grp.method = grp.method))  
+  } else if (grp.method == "textbook") { ## textbook method
+    if (is.null(match)) { # without matching
       ATE.est <- mean(Y[Z==1]) - mean(Y[Z==0])
-      ATE.var <- varCluster(Y[Z==1], grp[Z==1])$var + varCluster(Y[Z==0], grp[Z==0])$var
+      ATE.var <- varCluster(Y, Z, grp)$var
       return(list(est = ATE.est, var = ATE.var, Y = Y, Z = Z, grp = grp))         
     } else { # with matching
-      Y <- Ysum/size
+      Y <- Ysum/grp.size
       Y1 <- Y[Z==1]
       Y0 <- Y[Z==0]
-      w1 <- size[Z==1]
-      w0 <- size[Z==0]
+      w1 <- grp.size[Z==1]
+      w0 <- grp.size[Z==0]
       ATE.est <- weighted.mean(Y1, w1) - weighted.mean(Y0, w0)
       ind0 <- sort(match[Z==0], index.return = TRUE)
       ind1 <- sort(match[Z==1], index.return = TRUE)
@@ -130,6 +135,24 @@ ATEnocov <- function(Y, Z, data = parent.frame(), grp = NULL,
       return(list(est = ATE.est, var = ATE.var, Y = Y, Z = Z, grp = grp,
                   match = match))
     }  
+  } else if (grp.method == "unpooled") { # unpooled estimator
+    if (is.null(match)) {
+      ATE.est <- mean(Y[Z==1]) - mean(Y[Z==0])
+      ATE.var <- varCluster(Y[Z==1], grp[Z==1])$var + varCluster(Y[Z==0], grp[Z==0])$var
+      return(list(est = ATE.est, var = ATE.var, Y = Y, Z = Z, grp = grp))         
+    } else {
+      stop("this method is not available for matched-pair designs.") 
+    }
+  } else { # standard estimator
+    if (is.null(match)) {
+      ATE.est <- Ysum[Z==1]/sum(grp.size[Z==1]) - Ysum[Z==0]/sum(grp.size[Z==0])
+      ATE.var <- m1*var(Ysum[Z==1])/(sum(grp.size[Z==1])^2) +
+        m0*var(Ysum[Z==0])/(sum(grp.size[Z==0])^2)
+      return(list(est = ATE.est, var = ATE.var, Y = Y, Z = Z, grp = grp))         
+    } else {
+      stop("for matched-pair designs, use either `neyman' or `textbook' for `grp.method'") 
+    }
+    
   }
 }
 
