d90b6e14abc7a6387bfe95ba826169f09769d26f
kosukeimai <kimai@princeton.edu>
2007-03-07 16:21:34 +0000

    moved the function to randomize.R

--- R/randomize.R
+++ R/randomize.R
@@ -119,3 +119,26 @@ randomize <- function(data, group = c("Treat", "Control"), ratio =
   
   return(res)
 }
+
+###
+### This converts ratio into size while randomly allocating remainders 
+###
+
+ratio2size <- function(n, ratio, group) {
+  m <- length(ratio)
+
+  size <- round(ratio * n, digits = 0)
+  if (sum(size) > n) {
+    tmp <- sample(1:length(size), sum(size)-n, replace = FALSE)
+    size[tmp] <- size[tmp] - 1
+  }
+  if (sum(size) < n) {
+    tmp <- sample(1:length(size), n-sum(size), replace = FALSE)
+    size[tmp] <- size[tmp] + 1
+  }
+  allgroup <- NULL
+  for (i in 1:m)
+    allgroup <- c(allgroup, rep(group[i], size[i]))
+  return(list(size = size, vector = allgroup))
+  
+}
