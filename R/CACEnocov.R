bc334bbd6640e6cd35df311000a67697662ca916
kosukeimai <kimai@princeton.edu>
2007-03-07 02:16:47 +0000

    up

--- R/CACEnocov.R
+++ R/CACEnocov.R
@@ -1,14 +1,30 @@
 ###
-### Calculate the CACE with optional clustering
+### Calculate the CACE for individual and group randomized trials 
 ###
 
-CACEnocov <- function(Y, D, Z, grp = NULL){
+CACEnocov <- function(Y, D, Z, data = parent.frame(), grp = NULL,
+                      match = NULL, grp.size = NULL, grp.method = "neyman"){
+
+  ## get the data
+  call <- match.call()
+  Y <- eval(call$Y, envir = data)
+  D <- eval(call$D, envir = data)
+  Z <- eval(call$Z, envir = data)
+  grp <- eval(call$grp, envir = data)
+  grp.size <- eval(call$grp.size, envir = data)
+  match <- eval(call$match, envir = data)
   
-  N0 <- sum(Z==0)
-  N1 <- sum(Z==1)
-  ITTD <- ITTnocov(D, Z, grp)
-  ITTY <- ITTnocov(Y, Z, grp)
+  ## point estimates and variances
+  ITTY <- ATEnocov(Y = Y, Z = Z, grp = grp, match = match,
+                   grp.size = grp.size, grp.method = grp.method) 
+  ITTD <- ATEnocov(D = Y, Z = Z, grp = grp, match = match,
+                   grp.size = grp.size, grp.method = grp.method) 
+  CACEest <- ITTY$est/ITTD$est
+
+  ## covariance calculation
   if (is.null(grp)) {
+    N0 <- sum(Z==0)
+    N1 <- sum(Z==1)
     Cov0 <- cov(Y[Z==0], D[Z==0])/N0
     Cov1 <- cov(Y[Z==1], D[Z==1])/N1
   } else {
@@ -16,10 +32,42 @@ CACEnocov <- function(Y, D, Z, grp = NULL){
     Cov1 <- covCluster(Y[Z==1], D[Z==1], grp[Z==1])$cov
   }
   
-  est <- ITTY$est/ITTD$est
   var <- (ITTY$var*(ITTD$est^2) + ITTD$var*(ITTY$est^2) -
           2*(Cov0 + Cov1)*ITTY$est*ITTD$est)/(ITTD$est^4)
 
   return(list(est = est, var = var, ITTD = ITTD, ITTY = ITTY,
               cov = Cov0 + Cov1)) 
+}
+
+###
+### Calculate the cluster-adjusted covariance for two sample means 
+### the calculation is based on the formula analogous to the one
+### above.
+###
+
+covCluster <- function(Y1, Y2, grp) {
+  
+  n <- c(table(grp))
+  ugrp <- unique(grp)
+  k <- length(ugrp)
+  N <- length(Y1)
+  Y1bar <- mean(Y1)
+  Y2bar <- mean(Y2)
+  Y1gbar <- Y2gbar <- rep(NA, k)
+  MSw <- 0
+  ## within-group mean squares
+  for (i in 1:k) {
+    Y1gbar[i] <- mean(Y1[grp == ugrp[i]])
+    Y2gbar[i] <- mean(Y2[grp == ugrp[i]])
+    MSw <- MSw + sum((Y1[grp == ugrp[i]]-Y1gbar[i])*(Y2[grp == ugrp[i]]-Y2gbar[i]))
+  }
+  MSw <- MSw / (N-k)
+  ## between-group mean squares
+  MSb <- sum(n*(Y1gbar-Y1bar)*(Y2gbar-Y2bar))/(k-1)
+  ## intraclass correlation coefficient
+  n0 <- mean(n)-sum((n-mean(n))^2)/((k-1)*N)
+  rho <- (MSb - MSw)/(MSb + (n0-1)*MSw)
+  ## cluster-adjusted covariance
+  res <- var(Y1, Y2)*(1+(mean(n)-1)*rho)/N
+  return(list(cov = res, MSb = MSb, MSw = MSw, rho = rho))
 }
