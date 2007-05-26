12e14e7964eef460040396e3fec69b2fb92e63d5
kosukeimai <kimai@princeton.edu>
2007-05-26 01:14:33 +0000

    remove

--- R/onLoad.R
+++
@@ -1,8 +0,0 @@
-".onLoad" <- function(lib, pkg) {
-  mylib <- dirname(system.file(package = "experiment"))
-  title <- packageDescription("experiment", lib = mylib)$Title
-  ver <- packageDescription("experiment", lib = mylib)$Version
-  url <- packageDescription("experiment", lib = mylib)$URL
-  cat(title, "\nVersion:", ver, "\nURL:", url, "\n")
-}
-
