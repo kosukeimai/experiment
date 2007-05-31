682c90209db9e8bed461fdb27a34903f286d411a
kosukeimai <kimai@princeton.edu>
2007-05-31 02:58:29 +0000

    up

--- R/ATEcluster.R
+++ R/ATEcluster.R
@@ -10,13 +10,18 @@ ATEcluster <- function(Y, Z, grp, data, match = NULL,
 
   N <- length(Y)
   M <- length(unique(match))
-  Y1bar <- tapply(Y[Z==1], match, mean)
-  Y0bar <- tapply(Y[Z==0], match, mean)
-  Y1var <- tapply(Y[Z==1], match, var)/tapply(rep(1, sum(Z==1)),
-                      match, sum)
-  Y0var <- tapply(Y[Z==0], match, var)/tapply(rep(1, sum(Z==0)),
-                      match, sum)
+  Y1bar <- tapply(Y[Z==1], match[Z==1], mean)
+  Y0bar <- tapply(Y[Z==0], match[Z==0], mean)
+
+  ## conditional ATE
+  Y1var <- tapply(Y[Z==1], match[Z==1], var)/tapply(rep(1, sum(Z==1)),
+                      match[Z==1], sum)
+  Y0var <- tapply(Y[Z==0], match[Z==0], var)/tapply(rep(1, sum(Z==0)),
+                      match[Z==0], sum)
+  if (is.null(weights))
+    weights <- rep(1, N)
   w <- tapply(weights, match, mean)
+  w <- N*w/sum(w)
   ATE.est <- sum(w*(Y1bar-Y0bar))/N
   ATE.var <- sum(w^2*(Y1var+Y0var))/N^2
 
